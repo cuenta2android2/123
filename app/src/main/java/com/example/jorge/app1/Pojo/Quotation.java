cc1f23e6ddb68ffa8abc49414936dc9e75c16ae6
Jorge <jorgevicenteagustin@gmail.com>
2019-02-24 16:00:01 +0100

    123

---
+++ app/src/main/java/com/example/jorge/app1/Pojo/Quotation.java
@@ -0,0 +1,22 @@
+package com.example.jorge.app1.Pojo;
+
+public class Quotation {
+    private String quoteText;
+    private String quoteAuthor;
+
+    public String getQuote() {
+        return quoteText;
+    }
+
+    public void setQuote(String quote) {
+        this.quoteText = quote;
+    }
+
+    public String getAuthor() {
+        return quoteAuthor;
+    }
+
+    public void setAuthor(String author) {
+        this.quoteAuthor = author;
+    }
+}
---
+++ app/src/main/java/com/example/jorge/app1/adapters/ListAdapter.java
@@ -0,0 +1,46 @@
+package com.example.jorge.app1.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import com.example.jorge.app1.Pojo.Quotation;
+import com.example.jorge.app1.R;
+
+public class ListAdapter extends ArrayAdapter
+{
+    private Context context;
+    private int layout;
+    private List<Quotation> quotationList;
+
+    public ListAdapter(@NonNull Context context, int layout, List<Quotation> quotationList) {
+        super(context, layout, quotationList);
+        this.context = context;
+        this.layout = layout;
+        this.quotationList = quotationList;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.quotation_list_row, null);
+        }
+        TextView quoteTextView = (TextView) view.findViewById(R.id.tvQuote);
+        TextView authorTextView = (TextView) view.findViewById(R.id.tvAuthor);
+
+        quoteTextView.setText(this.quotationList.get(position).getQuote());
+        authorTextView.setText(this.quotationList.get(position).getAuthor());
+
+        return view;
+    }
+}
