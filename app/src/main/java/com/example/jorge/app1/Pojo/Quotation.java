7716ddd6ce429a9553758049cc34ac08cd1e6d9e
Jorge <jorgevicenteagustin@gmail.com>
2019-02-26 20:21:54 +0100

    456

--- app/src/main/java/com/example/jorge/app1/Pojo/Quotation.java
+++ app/src/main/java/com/example/jorge/app1/Pojo/Quotation.java
@@ -1,22 +1,49 @@
 package com.example.jorge.app1.Pojo;
 
 public class Quotation {
-    private String quoteText;
-    private String quoteAuthor;
+    String quoteText;
+    String quoteAuthor;
+    String senderName;
+    String senderLink;
+    String quoteLink;
 
     public String getQuote() {
         return quoteText;
     }
 
-    public void setQuote(String quote) {
-        this.quoteText = quote;
+    public void setQuote(String quoteText) {
+        this.quoteText = quoteText;
     }
 
     public String getAuthor() {
         return quoteAuthor;
     }
 
-    public void setAuthor(String author) {
-        this.quoteAuthor = author;
+    public void setAuthor(String quoteAuthor) {
+        this.quoteAuthor = quoteAuthor;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public String getSenderLink() {
+        return senderLink;
+    }
+
+    public void setSenderLink(String senderLink) {
+        this.senderLink = senderLink;
+    }
+
+    public String getQuoteLink() {
+        return quoteLink;
+    }
+
+    public void setQuoteLink(String quoteLink) {
+        this.quoteLink = quoteLink;
     }
 }
